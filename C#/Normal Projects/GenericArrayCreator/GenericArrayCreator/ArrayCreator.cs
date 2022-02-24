dd74ebeb67907a968de9852df08cb4acb6c2b313
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-02-24 08:58:19 +0200

    Add files via upload

---
+++ C#/Normal Projects/GenericArrayCreator/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] arr = new T[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = item;
+            }
+            return arr;
+        }
+    }
+}
