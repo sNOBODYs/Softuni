7427f22f35dc976c3417b4fc345263470a28859e
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-02-24 08:53:23 +0200

    Add files via upload

---
+++ C#/Normal Projects/Father/Father/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Father
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double boqc;
+            double boqb;
+
+                double ploshtPrednaStena = (a * a) - (1.2 * 2);
+                double ploshtDqsnaLqvaStrana = 2 * ((a * b) - (1.5 * 1.5));
+                double ploshtZadnaStena = a * a;
+                double ploshtPokkriv = 2 * ((a * h) / 2 + (a * b));
+
+
+            boqc = (ploshtPrednaStena + ploshtZadnaStena + ploshtDqsnaLqvaStrana) / 3.4;
+            boqb = ploshtPokkriv / 4.3;
+
+                Console.WriteLine($"{boqc:f2}");
+                Console.WriteLine($"{boqb:f2}");
+            }
+        }
+    }
+
+
---
+++ C#/Normal Projects/corpse/corpse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cropse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gardenS = double.Parse(Console.ReadLine());
+            double grapesP = double.Parse(Console.ReadLine());
+            double L = double.Parse(Console.ReadLine());
+            double rabotnici = double.Parse(Console.ReadLine());
+
+            double grapeS = grapesP * (gardenS * 0.40);
+            double litres = grapeS / 2.5;
+
+            if (litres > L)
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(litres)} liters.");
+                Console.WriteLine($"{Math.Ceiling(litres - L)} liters left -> {Math.Ceiling((litres -L) / rabotnici)} liters per person.");
+            }
+            else if (litres < L)
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(L - litres)} liters wine needed.");
+        }
+    }
+}
