3b11f60a97169d176d90b99a48d7837b05b4054b
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-02-25 08:14:49 +0200

    Add files via upload

---
+++ C#/List/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                           // 3#
+			List<int> nums = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			List<int> nums2 = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToList();
+
+			List<int> merged = new List<int>();
+
+			if (nums.Count >= nums2.Count)
+			{
+				for (int i = 0; i < nums.Count; i++)
+				{
+					merged.Add(nums[i]);
+					if (i < nums2.Count)
+					{
+						merged.Add(nums2[i]);
+					}
+				}
+			}
+			else
+			{
+				for (int i = 0; i < nums2.Count; i++)
+				{
+					if (i < nums.Count)
+					{
+						merged.Add(nums[i]);
+					}
+					merged.Add(nums2[i]);
+				}
+			}
+			Console.WriteLine(string.Join(" ", merged));
+
+		}
+	}
+}
---
+++ C#/List/ConsoleApp4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {                                              //6#
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                string[] tokens = line.Split();
+
+                switch (tokens[0])
+                {
+                    case "Add":
+                        int numberAdd = int.Parse(tokens[1]);
+                        numbers.Add(numberAdd);
+                        break;
+                    case "Remove":
+                        int numberRemove = int.Parse(tokens[1]);
+                        numbers.Remove(numberRemove);
+                        break;
+                    case "RemoveAt":
+                        int removeAt = int.Parse(tokens[1]);
+                        numbers.RemoveAt(removeAt);
+                        break;
+                    case "Insert":
+                        int num = int.Parse(tokens[1]);
+                        int index = int.Parse(tokens[2]);
+                        numbers.Insert(index, num);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
