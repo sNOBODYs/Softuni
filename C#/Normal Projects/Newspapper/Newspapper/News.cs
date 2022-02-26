8263c5739b607d961cca3ed942a8356009247fc7
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-02-26 13:04:15 +0200

    Add files via upload

---
+++ C#/Normal Projects/Newspapper/Newspapper/News.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newspapper
+{
+    class News
+    {
+        private string title;
+        private string text;
+        private string writer;
+
+        public string Title
+        {
+            get { return title; }
+            set { this.title = value; }
+        }
+        public string Text
+        {
+            get { return text; }
+            set { this.text = value; }
+        }
+        public string Writer
+        {
+            get { return writer; }
+            set { this.writer = value; }
+        }
+        public News(string text, string writer, string title)
+        {
+            this.text = text;
+            this.writer = writer;
+            this.title = title;
+        }
+        public void Edit(string newText)
+        {
+            this.Text = newText;
+        }
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+        public void ChangeWriter(string newWriter)
+        {
+            this.Writer = newWriter;
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Text}: {this.Writer}";
+        }
+
+    }
+}
---
+++ C#/Normal Projects/Newspapper/Newspapper/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Newspapper
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(",").ToArray();
+            string title = input[0];
+            string text = input[1];
+            string writer = input[2];
+
+            News news = new News(title, text, writer);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] commandPlusInput = Console.ReadLine().Split(": ");
+                string command = commandPlusInput[0];
+
+                if (command == "Edit")
+                {
+                    news.Edit(commandPlusInput[1]);
+                }
+                else if (command == "ChangeWriter")
+                {
+                    news.ChangeWriter(commandPlusInput[1]);
+                }
+                else if (command == "Rename")
+                {
+                    news.Rename(commandPlusInput[1]);
+                }
+            }
+            Console.WriteLine(news.ToString());
+        }
+    }
+}
