b46be0949ea437409eb16153febab814a630ed05
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-02-26 13:08:30 +0200

    Add files via upload

---
+++ C#/Normal Projects/Radians to degress/Radians to degress/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radians_to_degress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double gr = (2 * Math.PI) / 360;
+            Console.WriteLine(Math.Round(gr));
+        }
+    }
+}
