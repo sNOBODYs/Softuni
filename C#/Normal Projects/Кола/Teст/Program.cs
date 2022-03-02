256fdeccc0df3eb04793e0492c53535811ed8c07
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-02 09:34:20 +0200

    Add files via upload

---
+++ C#/Normal Projects/Кола/Teст/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Teст
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Bees = int.Parse(Console.ReadLine());
+            int healthB = int.Parse(Console.ReadLine());
+            int attackB = int.Parse(Console.ReadLine());
+
+            while (healthB >= 0 && Bees > 100)
+            {
+
+                Bees = Bees - attackB;
+                healthB = healthB - Bees * 5;
+
+                if (healthB <= 0)
+                {
+                    Console.WriteLine("Beehive won! Bees left " + Bees + ".");
+                }
+                if (Bees < 100)
+                {
+                    if (Bees < 0)
+                    {
+                        Console.WriteLine("The bear stole the honey! Bees left 0.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The bear stole the honey! Bees left " + Bees + ".");
+                    }
+                }
+            }
+
+        }
+
+
+       
+    }
+}
---
+++ C#/Normal Projects/Кола/Кола/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Кола
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pari = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string classK = "";
+            string vidK = "";
+
+            if (pari <= 100)
+            {
+                classK = "Economy class";
+                switch (season)
+                {
+                    case "Summer":
+                        pari = pari * 0.35;
+                        vidK = "Cabrio";
+                        break;
+                    case "Winter":
+                        pari = pari * 0.65;
+                        vidK = "Jeep";
+                        break;
+                }
+
+            }
+            else if (pari >= 100 && pari <= 500)
+            {
+                classK = "Compact class";
+                switch (season)
+                {
+                    case "Summer":
+                        pari = pari * 0.45;
+                        vidK = "Cabrio";
+                        break;
+                    case "Winter":
+                        pari = pari * 0.80;
+                        vidK = "Jeep";
+                        break;
+                }
+            }
+            else if (pari > 500)
+            {
+                classK = "Luxury class";
+                switch (season)
+                {
+                    case "Summer":
+                    case "Winter":
+                        pari = pari * 0.9;
+                        vidK = "Jeep";
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{classK}");
+            Console.WriteLine($"{vidK} - {pari:f2}");
+
+        }
+    }
+}
