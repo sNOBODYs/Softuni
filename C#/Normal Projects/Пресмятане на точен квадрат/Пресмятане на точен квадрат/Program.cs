390317e86ead46fa15ccb560adcdd44dd533be7a
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-02 10:01:07 +0200

    Add files via upload

---
+++ C#/Normal Projects/Пресмятане на точен квадрат/Пресмятане на точен квадрат/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Пресмятане_на_точен_квадрат
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = int.Parse(Console.ReadLine());
+			int area = a * a;
+			Console.WriteLine(area);
+		}
+	}
+}
