68af3855c5540547d30f349f0c7706af3d255ca9
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-02 10:00:02 +0200

    Add files via upload

---
+++ C#/Normal Projects/Книгата на Наков/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Console.WriteLine(Repeater(words));
+        }
+
+        static string Repeater(string[] words)
+        {
+            StringBuilder newWord = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                foreach (char ch in word)
+                {
+                    newWord.Append(word);
+                }
+            }
+
+            return newWord.ToString();
+        }
+    }
+}
