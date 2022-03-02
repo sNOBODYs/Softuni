adcac8288bd1176e50c5a10d3c1f4ad68c660341
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-02 09:12:01 +0200

    Add files via upload

---
+++ C#/Normal Projects/Tuple/Tuple/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tuple<T1, T2> pearsonInfo = new Tuple<T1, T2>();
+        }
+    }
+}
---
+++ C#/Normal Projects/Tuple/Tuple/Tuple.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    class Tuple<T1,T2>
+    {
+
+    }
+}
