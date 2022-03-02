e3727ee5177fb5aa6f69a0172d0172b469f65a1b
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-02 10:03:33 +0200

    Add files via upload

---
+++ C#/Normal Projects/Текстообработка/Текстообработка/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Текстообработка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string input = Console.ReadLine().ToLower();
+            string partOf = Console.ReadLine().ToLower() ;
+            int index = input.IndexOf(partOf);
+
+            while (index != -1)
+            {
+                count++;
+                index = input.IndexOf(partOf, index + 1);
+            }
+            Console.WriteLine(count);
+               
+            
+        }
+    }
+}
