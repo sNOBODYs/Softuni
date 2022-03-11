8557f4edeb963eacbfc62b89e03730ac472493d3
sNOBODYs <80042389+sNOBODYs@users.noreply.github.com>
2022-03-11 10:07:03 +0200

    Add files via upload

---
+++ C#/Normal Projects/Intro to Data Structures/Bag of Words/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+namespace Bag_of_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedBag<string> bag = new OrderedBag<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                bag.Add(word);
+            }
+
+            foreach (var element in bag)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ C#/Normal Projects/Intro to Data Structures/Intro to Data Structures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intro_to_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+            string thirdWord = Console.ReadLine();
+            string fourthWord = Console.ReadLine();
+            list.AddFirst(firstWord);
+            list.AddLast(secondWord);
+            list.AddAfter(list.First, thirdWord);
+            list.AddBefore(list.Last, fourthWord);
+
+            Console.WriteLine(string.Join(",",list));
+        }
+    }
+}
